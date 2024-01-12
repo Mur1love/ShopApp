3e14031d883ea3c17f412c7bd858cec662b7a176
Mur1love <murilormleal@outlook.com>
2024-01-12 19:21:22 -0300

    correção de rotas do app Drawer

--- lib/components/app_drawer.dart
+++ lib/components/app_drawer.dart
@@ -19,7 +19,7 @@ class AppDrawer extends StatelessWidget {
             leading: const Icon(Icons.shop),
             title: const Text("Loja"),
             onTap: () {
-              Navigator.of(context).pushReplacementNamed(AppRoutes.ORDERS);
+              Navigator.of(context).pushReplacementNamed(AppRoutes.HOME);
             },
           ),
           const Divider(),
