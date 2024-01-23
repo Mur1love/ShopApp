6e4ee5907110d751d439df5c4027c506ab13428f
Mur1love <murilormleal@outlook.com>
2024-01-23 19:04:16 -0300

    sliver animation on product detail

--- lib/components/product_grid_item.dart
+++ lib/components/product_grid_item.dart
@@ -33,10 +33,13 @@ class ProductGridItem extends StatelessWidget {
               arguments: product,
             );
           },
-          child: FadeInImage(
-            placeholder: const AssetImage('assets/images/product_placeholder.jpg'),
-            image: NetworkImage(product.imgUrl),
-            fit: BoxFit.cover,
+          child: Hero(
+            tag: product.id,
+            child: FadeInImage(
+              placeholder: const AssetImage('assets/images/product_placeholder.jpg'),
+              image: NetworkImage(product.imgUrl),
+              fit: BoxFit.cover,
+            ),
           ),
         ),
         footer: GridTileBar(
