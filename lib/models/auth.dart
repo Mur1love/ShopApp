dbd31e157885eeabb8347fbf6e607a090fe02deb
Mur1love <murilormleal@outlook.com>
2024-01-23 17:12:25 -0300

    fix bug register & tryautologin

--- lib/models/auth.dart
+++ lib/models/auth.dart
@@ -89,7 +89,7 @@ class Auth with ChangeNotifier {
 
     _token = userData['token'];
     _email = userData['email'];
-    _userId = userData['userid'];
+    _userId = userData['userId'];
     _expiryDate = expiryDate;
 
     _autoLogout();
@@ -103,7 +103,10 @@ class Auth with ChangeNotifier {
     _userId = null;
     _expiryDate = null;
     _clearLogoutTimer();
-    notifyListeners();
+    Store.remove('userData').then((_) {
+      notifyListeners();
+    });
+    
   }
 
   void _clearLogoutTimer() {
